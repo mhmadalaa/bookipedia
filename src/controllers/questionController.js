da4341f764dcd83c58491cb3af64c9897e25399d
mhmadalaa <mhmaaddalaa@gmail.com>
2024-01-28 17:17:12 +0200

    feat: brain storme the requirements

---
+++ src/controllers/questionController.js
@@ -0,0 +1,11 @@
+// question on document
+// question on book
+// then stream the response
+
+// document model?
+// book model?
+
+// fetch the request and make it more informative
+// make another request to ai
+// stream response if it done with ai-api
+// return invalid response
