9c7fa6970a2f2d663c8b7c05e2168c9b12cfbf2f
lilhind <130204568+lilhind@users.noreply.github.com>
2024-01-29 15:17:51 +0000

    chore: adding route handler

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const app = require('./src/app');
 
+app.get('/', (req, res) => {
+    // This code will be executed when a GET request is made to the base URL
+    res.send('welcome to bookipedia app');
+  });
 
 app.listen(3000, () => {
     console.log('server is running on port 3000...')
