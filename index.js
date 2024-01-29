be8fa88861d4149d413ef61b26b5a7184fc15334
mhmadalaa <mhmaaddalaa@gmail.com>
2024-01-29 18:47:14 +0200

    fix: solve files conflicts like `index.js` `package.json`

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+const mongoose = require('mongoose');
 const app = require('./src/app');
 
-app.get('/', (req, res) => {
-    // This code will be executed when a GET request is made to the base URL
-    res.send('welcome to bookipedia app');
+//to handle unhandled promise rejections
+process.on('unhandledRejection', (error) => {
+  console.log('unhandledRejection', error.message);
+});
+
+// CONNECT TO DATABASE
+const url = process.env.DATABASE;
+mongoose
+  .connect(url)
+  .then((con) => {
+    console.log('DB connecting successful...');
+  })
+  .catch((err) => {
+    console.log('DB connection ERROR!!');
+    console.log(err);
   });
 
+app.get('/', (req, res) => {
+  // This code will be executed when a GET request is made to the base URL
+  res.send('welcome to bookipedia app');
+});
+
+// START SERVER
 app.listen(3000, () => {
-    console.log('server is running on port 3000...')
+  console.log('server is running on port 3000...');
 });
 
 // Allow Vercel to turn Express into a serverless function
--- src/app.js
+++ src/app.js
@@ -1,7 +1,34 @@
+require('dotenv').config();
+
 const express = require('express');
+const morgan = require('morgan');
+
 const auth = require('./routers/auth');
+const booksRouter = require('./routers/booksRouter');
+
 const app = express();
+
 app.use(express.json());
-app.use('/auth', auth)
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  }),
+);
+
+// APP ROUTERS
+app.use('/', booksRouter);
+app.use('/auth', auth);
+
+// NOT FOUND ROUTERS ERROR HANDLER
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    error: {
+      message: err.message,
+    },
+  });
+});
 
 module.exports = app;
