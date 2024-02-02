b67bbf17ac2342891bc39473ea48a83493cd3d30
mhmadalaa <mhmaaddalaa@gmail.com>
2024-02-03 00:17:01 +0200

    feat: model chapter summaries

---
+++ src/models/chSummaryModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const chSummarySchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  content: {
+    type: String,
+    required: [true, 'provide the content of the summary'],
+  },
+  book: {
+    type: String, // FIXME: temporary
+    // type: mongoose.Schema.ObjectId,
+    // ref: 'Book',
+    required: [true, 'please specifiy the book.'],
+  },
+  chapter: {
+    type: Number,
+    required: [true, 'please specifiy the chapter of the book'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const ChapterSummary = mongoose.model('ChapterSummary', chSummarySchema);
+
+module.exports = ChapterSummary;
