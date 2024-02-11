9221d2837c1bb3b59b99e639ac269fc5cbbbea76
mhmadalaa <mhmaaddalaa@gmail.com>
2024-02-11 02:39:10 +0200

    feat: add geting chapters summaries different cases

--- src/controllers/chSummaryController.js
+++ src/controllers/chSummaryController.js
@@ -1,5 +1,8 @@
 const catchAsync = require('../utils/catchAsync');
 const ChapterSummary = require('../models/chSummaryModel');
+const AppError = require('../utils/appError');
+
+// TODO: update ch summary controller
 
 exports.createChapterSummary = catchAsync(async (req, res) => {
   const summary = await ChapterSummary.create(req.body);
@@ -10,12 +13,34 @@ exports.createChapterSummary = catchAsync(async (req, res) => {
   });
 });
 
-// TODO: it's needed to know the number of chapters in each book from book model
-exports.getChapterSummary = catchAsync(async (req, res) => {
+exports.getChapterSummary = catchAsync(async (req, res, next) => {
+  const bookChapters = 15; // FIXME: assume that all books has `15` chapters until we connect with book model
+
+  // case of unavilable chapter
+  if (req.params.chapter <= 0 || req.params.chapter > bookChapters) {
+    return next(
+      new AppError(
+        `That is not an avilable chapter in this book, availabe from ${1} to ${bookChapters}`,
+        404,
+      ),
+    );
+  }
+
+  // query book chapters summary
   const summary = await ChapterSummary.findOne({
     book: req.params.book_id,
     chapter: req.params.chapter,
   });
+
+  // case where AI not generate summary for this chapter
+  if (summary === null) {
+    return next(
+      new AppError(
+        'Sorry, summary for this chapter not avilabe right now.',
+        404,
+      ),
+    );
+  }
 
   res.status(200).json({
     message: 'success',
