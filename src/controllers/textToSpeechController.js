32ce4998fa25c8b3c329f5ebc38024869283e822
mhmadalaa <mhmaaddalaa@gmail.com>
2024-02-12 02:14:15 +0200

    feat: handle textToSpeech errors with catchAsync

--- src/controllers/textToSpeechController.js
+++ src/controllers/textToSpeechController.js
@@ -1,32 +1,26 @@
 const stream = require('stream');
 
 const openai = require('./../config/openaiConfig');
+const catchAsync = require('./../utils/catchAsync');
 
-exports.textToSpeech = async (req, res) => {
-  try {
-    const response = await openai.audio.speech.create({
-      model: 'tts-1',
-      voice: 'alloy',
-      input: req.body.text,
-    });
+exports.textToSpeech = catchAsync(async (req, res) => {
+  const response = await openai.audio.speech.create({
+    model: 'tts-1',
+    voice: 'alloy',
+    input: req.body.text,
+  });
 
-    // Set the appropriate headers for the response
-    res.setHeader('Content-Type', 'audio/mpeg');
-    res.setHeader('Content-Disposition', 'attachment; filename=speech.mp3');
+  // Set the appropriate headers for the response
+  res.setHeader('Content-Type', 'audio/mpeg');
+  res.setHeader('Content-Disposition', 'attachment; filename=speech.mp3');
 
-    // Convert the base64 audio content to a Buffer
-    const buffer = Buffer.from(await response.arrayBuffer());
+  // Convert the base64 audio content to a Buffer
+  const buffer = Buffer.from(await response.arrayBuffer());
 
-    // Create a readable stream from the buffer
-    const audioStream = new stream.PassThrough();
-    audioStream.end(buffer);
+  // Create a readable stream from the buffer
+  const audioStream = new stream.PassThrough();
+  audioStream.end(buffer);
 
-    // Pipe the audio stream to the response
-    audioStream.pipe(res);
-  } catch (err) {
-    // console.error('Error fetching TTS response:', err);
-    res.status(500).json({
-      error: 'Internal server error',
-    });
-  }
-};
+  // Pipe the audio stream to the response
+  audioStream.pipe(res);
+});
