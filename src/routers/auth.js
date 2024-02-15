979f104d2396dead957dc290d018d15def767c35
lilhind <130204568+lilhind@users.noreply.github.com>
2024-02-15 08:59:32 +0000

    test: connecting to db

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,5 +1,9 @@
 const express = require('express');
+require('dotenv').config(); // TODO: remove this line
 
+
+
+/*
 const router = express();
 
 router.get('/', (req, res) => {
@@ -8,3 +12,31 @@ router.get('/', (req, res) => {
 
 
 module.exports = router;
+*/
+
+
+// Replace the uri string with your connection string.
+
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your connection string.
+const uri = process.env.DATABASE;
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const database = client.db('<db>');
+    const users = database.collection('users');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { username: 'lilhind' };
+    const user = await users.findOne(query);
+
+    console.log(user);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
