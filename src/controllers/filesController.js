e7e3870803d82eda889ae021ad7a88b1ce7a4f6a
mhmadalaa <mhmaaddalaa@gmail.com>
2024-03-05 22:54:11 +0200

    refactor filecontroller code

--- src/controllers/filesController.js
+++ src/controllers/filesController.js
@@ -1,13 +1,11 @@
+const mongoose = require('mongoose');
 const { GridFSBucket, MongoClient } = require('mongodb');
 // const {bookipediaConnection} = require('./../db/connections');
 const catchAsync = require('../utils/catchAsync');
-// const { ObjectId } = require('bson');
 const CoverImageModel = require('./../models/ImageModel');
-const mongoose = require('mongoose');
 
 const db = mongoose.connection;
 const bucket = new GridFSBucket(db);
-
 // const bucket = new GridFSBucket(bookipediaConnection);
 
 const url = process.env.DATABASE;
@@ -18,8 +16,8 @@ exports.uploadFile = catchAsync(async (req, res, next) => {
     deleteImage(req, res, next);
     return res.status(400).json({ error: 'No file uploaded' });
   }
+
   const fileBuffer = req.files.file[0].buffer; // Access the file buffer
-  // const filename = `${Date.now()}${req.files.file[0].originalname}`;
   const filename = req.files.file[0].originalname;
   const uploadStream = bucket.openUploadStream(filename);
   const id = uploadStream.id;
@@ -53,30 +51,31 @@ exports.uploadImage = catchAsync(async (req, res, next) => {
 });
 
 exports.displayFile = catchAsync(async (req, res, next) => {
-  const files = await client.db().collection('fs.files');
-  // const fileId = new ObjectId(req.fileId);
-  const fileId = req.fileId;
+  const files = client.db().collection('fs.files');
+  const fileId = new mongoose.Types.ObjectId(req.fileId);
   const file = await files.findOne({ _id: fileId });
+
   if (!file) {
     return res.status(404).json({
       message: 'File not found',
     });
   }
+
   const downloadStream = bucket.openDownloadStream(fileId);
 
   // Set appropriate headers
   res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', 'inline; filename=example.pdf');
+  res.setHeader('Content-Disposition', `inline; filename=${file.filename}`);
 
   // Pipe the file stream to the response
   downloadStream.pipe(res);
 });
 
 exports.deleteFile = catchAsync(async (req, res, next) => {
-  const files = await client.db().collection('fs.files');
-  const chuncks = await client.db().collection('fs.chunks');
-  // const fileId = new ObjectId(req.fileId);
-  const fileId = req.fileId;
+  const files = client.db().collection('fs.files');
+  const chuncks = client.db().collection('fs.chunks');
+  const fileId = new mongoose.Types.ObjectId(req.fileId);
+
   await files.deleteOne({ _id: fileId });
   await chuncks.deleteMany({ files_id: fileId });
 });
