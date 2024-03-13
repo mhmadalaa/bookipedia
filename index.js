ba556adb03ce9d048e73806448245047b70c755e
lilhind <130204568+lilhind@users.noreply.github.com>
2024-03-13 23:08:55 +0000

    added /shell endpoint

--- index.js
+++ index.js
@@ -21,8 +21,29 @@ app.get('/', (req, res) => {
   res.send('welcome to bookipedia app.');
 });
 
-app.get('/deploy', function(req, res) {
-  res.send('This is a GET request');
+// deployment setup
+const shell = require('shelljs');
+app.post('/deploy', function(req, res) {
+  const deployment_key_req = req.body.secret_key;
+
+  if (deployment_key_req && deployment_key_req === process.env.DEPLOYMENT_KEY) {
+    // Log the current working directory
+    console.log('Current working directory:', process.cwd());
+
+    // Make deploy.sh executable
+    shell.chmod('+x', './deploy.sh');
+
+    // Execute deploy.sh
+    shell.exec('./deploy.sh', (code, stdout, stderr) => {
+      console.log('Exit code:', code);
+      console.log('Program output:', stdout);
+      console.log('Program stderr:', stderr);
+    });
+
+    res.status(200).send('Deployment script executed');
+  } else {
+    res.status(403).send('Unauthorized');
+  }
 });
 
 // START SERVER
