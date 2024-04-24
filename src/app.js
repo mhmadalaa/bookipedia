7e4d15bf61c9957434d7591e214366ec23dbba4f
Mohamed Alaa <mhmaaddalaa@gmail.com>
2024-04-24 17:57:37 +0200

    Revert "feat: admin authority and functionality (#59)" (#66)
    This reverts commit 2b7be3c80681742b3ff78528e32bd0b5237dc4f9.

--- src/app.js
+++ src/app.js
@@ -6,12 +6,9 @@ const authRouter = require('./routers/authRoute');
 const bookRouter = require('./routers/bookRoute');
 const aiRouter = require('./routers/aiRoute');
 const documentRouter = require('./routers/documentRoute');
+const noteRouter = require('./routers/noteRoute');
 const deployRouter = require('./routers/deployRoute');
-const adminRouter = require('./routers/adminRoute');
 
-// BACKGROUND TASKS
-require('./background_tasks/nonVerifiedUsers');
-require('./background_tasks/dailyAdminsOtp');
 
 const app = express();
 
@@ -25,14 +22,14 @@ app.use(
   express.urlencoded({
     extended: false,
   }),
-);
+); 
 
 // APP ROUTERS
 app.use('/document', documentRouter);
 app.use('/book', bookRouter);
 app.use('/auth', authRouter);
 app.use('/ai', aiRouter);
-app.use('/admin', adminRouter);
+app.use('/note', noteRouter);
 
 // deployement router managed by github-workflow
 app.use('/deploy', deployRouter);
