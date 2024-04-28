7eeed6867560e7d7c5e5d17ae5874dd93f3b0828
mhmadalaa <mhmaaddalaa@gmail.com>
2024-04-28 05:39:46 +0300

    document backend endpoints that updated by ai-api

--- src/controllers/AI_APIController.js
+++ src/controllers/AI_APIController.js
@@ -20,6 +20,27 @@ exports.addFileToAI = async (req, res, next) => {
   }
 };
 
+/*
+  ocr-pdf endpoint to accept the updated document pdf that ocr applied to it
+
+  router in AI_APIRoute.js can be  `POST /ocr-pdf/:pdf_id`
+  pdf_id: is the pdf id sent from ai-api
+  so we will: 
+    uplaod the pdf to gridfs and hold the new id as example: `req.update_fileId = _id`
+    then ⟹ search in `Document` model wehere `file_id = req.params.pdf_id`
+    then ⟹ remove the old pdf and set the `file_id: req.update_fileId`
+*/
+
+/*
+  update chat_summary endpoint to accept the updated chat_summary with each new question
+
+  router in AI_APIRoute.js can be  `PATCH /chat-summary/:chat_id`
+  chat_id is sent from ai-api which is the Chat model id
+  we wil: 
+    search in Chat model with `req.params.chat_id`
+    and update the content of chat_summary in it 
+*/
+
 // serve pdf files to ai-api
 exports.serveFile = async (req, res, next) => {
   req.fileId = req.params.id;
