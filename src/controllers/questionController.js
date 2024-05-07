1490de8e0dc58d1697a0c98a29f1564f83112721
mhmadalaa <mhmaaddalaa@gmail.com>
2024-05-08 01:50:39 +0300

    add sayed darwesh poem

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -11,65 +11,88 @@ const AI_API = process.env.AI_API;
 
 exports.askQuestion = catchAsync(async (req, res) => {
   // ai-request query parameters
-  const queryParams = {
-    enable_web_retrieval: true,
-  };
+  // const queryParams = {
+  //   enable_web_retrieval: true,
+  // };
 
   const chat = await chatAndFileId(req);
 
   req.chat_id = chat.chat._id;
-  const last_questions = await lastQuestions(req);
+  // const last_questions = await lastQuestions(req);
 
-  const dataToSend = {
-    user_prompt: req.body.question,
-    chat_summary: chat.chat.chat_summary,
-    chat: last_questions.join(', '),
-    doc_ids: [chat.file_id.toString()],
-  };
+  const chat_answer =
+    'aho dh ally sar, oady ally kan. mlksh 78, mlksh 78 tlom 3lya. tlom 3lya azay ya sydna, o5yr bldna mhosh fy aydna, tlom 3lya azay ya sydna,o5yr bldna mhosh fy aydna, 8oly 3n ashya2 tfydna, o b3dha b8y lom 3lya';
 
-  const bufferStream = new BufferListStream();
+  const chat_sources = `[{"doc_id": "663194026c43875d7ee26590","page_no": 16,"text": "sayed darwesh"}, "https://arz.wikipedia.org/wiki/%D8%A7%D9%87%D9%88_%D8%AF%D9%87_%D8%A7%D9%84%D9%84%D9%89_%D8%B5%D8%A7%D8%B1"]`;
 
-  try {
-    axios
-      .get(`${AI_API}/chat_response/${req.chat_id.toString()}`, {
-        params: queryParams,
-        data: dataToSend,
-        responseType: 'stream',
-      })
-      .then(async (response) => {
-        // accumulate the response data to a buffer list
-        response.data.on('data', (chunk) => {
-          // Append each chunk to the buffer
-          bufferStream.append(Buffer.from(chunk));
-        });
+  await Question.create({
+    question: req.body.question,
+    answer: chat_answer,
+    sources: chat_sources,
+    chat_id: req.chat_id,
+    user: req.user._id,
+    createdAt: Date.now(),
+  });
 
-        // pipe the response stream to client
-        await pipeline(response.data, res);
+  res.send(`{
+    "response": "aho dh ally sar, oady ally kan. mlksh 78, mlksh 78 tlom 3lya. tlom 3lya azay ya sydna, o5yr bldna mhosh fy aydna, tlom 3lya azay ya sydna,o5yr bldna mhosh fy aydna, 8oly 3n ashya2 tfydna, o b3dha b8y lom 3lya",
 
-        const ai_response = bufferStream.toString();
+"sources": "[{
+  "doc_id": "663194026c43875d7ee26590",
+  "page_no": 16,
+  "text": "sayed darwesh"
+}, "https://arz.wikipedia.org/wiki/%D8%A7%D9%87%D9%88_%D8%AF%D9%87_%D8%A7%D9%84%D9%84%D9%89_%D8%B5%D8%A7%D8%B1"]" }`);
 
-        const json_response = JSON.parse(ai_response);
+  // const dataToSend = {
+  //   user_prompt: req.body.question,
+  //   chat_summary: chat.chat.chat_summary,
+  //   chat: last_questions.join(', '),
+  //   doc_ids: [chat.file_id.toString()],
+  // };
 
-        const chat_answer = JSON.stringify(json_response.response);
-        const chat_sources = JSON.stringify(json_response.sources);
+  // const bufferStream = new BufferListStream();
 
-        // save the question data to database
-        await Question.create({
-          question: req.body.question,
-          answer: chat_answer.toString(),
-          sources: chat_sources.toString(),
-          chat_id: req.chat_id,
-          user: req.user._id,
-          createdAt: Date.now(),
-        });
-      });
-  } catch (error) {
-    console.error('✗ Error while answering a chat question', error);
-    res.status(500).json({
-      message: '✗ Error while answering a chat question',
-      error: error.message,
-    });
-  }
+  // try {
+  //   axios
+  //     .get(`${AI_API}/chat_response/${req.chat_id.toString()}`, {
+  //       params: queryParams,
+  //       data: dataToSend,
+  //       responseType: 'stream',
+  //     })
+  //     .then(async (response) => {
+  //       // accumulate the response data to a buffer list
+  //       response.data.on('data', (chunk) => {
+  //         // Append each chunk to the buffer
+  //         bufferStream.append(Buffer.from(chunk));
+  //       });
+
+  //       // pipe the response stream to client
+  //       await pipeline(response.data, res);
+
+  //       const ai_response = bufferStream.toString();
+
+  //       const json_response = JSON.parse(ai_response);
+
+  //       const chat_answer = JSON.stringify(json_response.response);
+  //       const chat_sources = JSON.stringify(json_response.sources);
+
+  //       // save the question data to database
+  //       await Question.create({
+  //         question: req.body.question,
+  //         answer: chat_answer.toString(),
+  //         sources: chat_sources.toString(),
+  //         chat_id: req.chat_id,
+  //         user: req.user._id,
+  //         createdAt: Date.now(),
+  //       });
+  //     });
+  // } catch (error) {
+  //   console.error('✗ Error while answering a chat question', error);
+  //   res.status(500).json({
+  //     message: '✗ Error while answering a chat question',
+  //     error: error.message,
+  //   });
+  // }
 });
 
 exports.reteriveChat = catchAsync(async (req, res) => {
